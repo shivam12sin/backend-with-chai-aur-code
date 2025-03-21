e871d3499a062354032b0444c30325759b7f7699
shivam12sin <shivam.23scse1010271@galgotiasuniversity.ac.in>
2025-03-21 19:58:56 +0530

    connect to mongoatlas

---
+++ src/index.js
@@ -0,0 +1,36 @@
+// require('dotenv').config({path:'./env'})
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+
+dotenv.config({
+  path:'./env'
+})
+
+
+
+connectDB()
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express()
+( async()=>{
+  try {
+   await mongoose.connect(`${process.env.MONGODB_URI}/$${DB_NAME}`)
+   app.om("error",(error)=>{
+    console.log("ERROR",error);
+    throw error
+
+   })
+    
+  } catch (error) {
+    console.error("ERROR",error)
+    throw err
+    
+  }
+})() */
